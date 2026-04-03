a343a030a9809731304cfbc5b5440efa5b01e03e
Tugu <200585649@student.georgianc.on.ca>
2026-04-03 16:32:33 -0400

    first step for my part is get elements from student1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Tuguldur Natsagdorj 200585649
+//step 4 and 5
+
+//get elements
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
