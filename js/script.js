7eb167cdb152216cc316c4ae76a845980abd3357
Tugu <200585649@student.georgianc.on.ca>
2026-04-03 16:45:08 -0400

    added elements to li, li to list then input

--- js/script.js
+++ js/script.js
@@ -9,27 +9,37 @@ const taskList = document.getElementById("taskList");
 //add button event
 addBtn.addEventListener("click", function(){
     const taskText = taskInput.value.trim();
+    
     //prevent empty taks
     if (taskText === "") return;
+    
     //create list
     const li = document.createElement("li");
+    
     //create checkbox
     const checkbox = document.createElement("input");
+
     checkbox.type = "checkbox";
+    
     //create span for text
     const span = document.createElement("span");
     span.textContent = taskText;
+    
     //create delete button
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
+    
     //checkbox event (change)
     checkbox.addEventListener("change", function () {
         if (checkbox.checked) {
+          
             //add line-through
             span.style.textDecoration = "line-through";
+          
             //move to bottom
             taskList.appendChild(li);
         } else {
+          
             //remove line-through
             span.style.textDecoration = "none";
         }
@@ -39,3 +49,15 @@ addBtn.addEventListener("click", function(){
     deleteBtn.addEventListener("click", function () {
         li.remove();
     });
+ 
+    //addd elements to li
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    //add li to list
+    taskList.appendChild(li);
+
+    // input
+    taskInput.value = "";
+});
